83af4b40ce80d886983635c6a2387bdd23b77b66
zainheraswada <zainheraswada@gmail.com>
2024-11-15 14:43:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-<div id="thesports_n4ho4l5yjou9upbo"></div><script>(function(w,s,p,o,r,t){r=document.createElement(s);t=document.getElementsByTagName(s)[0];r.async=true;r.src=p;t.parentNode.insertBefore(r,t);r.onload=()=>{w['thesports']?.setProfile(o.profile,o.options);};})(window,'script',`//cdn-saas.thesports.com/loader.umd.js?t=${parseInt(Date.now()/1e7)}`,{profile:{profile_id:"n4ho4l5yjou9upbo",sport:"football",widget_id:"match_overview",match_id:"8yomo4h39836q0j",lang:"en",},options:{theme:"auto",color_primary_light:"#FF0100",color_bg_light:"#FFFFFF",color_text_light:"#050917",color_inplay_light:"#FF0100",color_primary_dark:"#FF0100",color_bg_dark:"#050917",color_text_dark:"#FFFFFF",color_inplay_dark:"#FF0100",}});</script>
+(function (w, s, p, o, r, t) {
+    r = document.createElement(s);
+    t = document.getElementsByTagName(s)[0];
+    r.async = true;
+    r.src = p;
+    t.parentNode.insertBefore(r, t);
+    r.onload = () => {
+        w['thesports']?.setProfile(o.profile, o.options);
+    };
+})(
+    window,
+    'script',
+    `//cdn-saas.thesports.com/loader.umd.js?t=${parseInt(Date.now() / 1e7)}`,
+    {
+        profile: {
+            profile_id: "n4ho4l5yjou9upbo",
+            sport: "football",
+            widget_id: "match_overview",
+            match_id: "8yomo4h39836q0j",
+            lang: "en",
+        },
+        options: {
+            theme: "auto",
+            color_primary_light: "#FF0100",
+            color_bg_light: "#FFFFFF",
+            color_text_light: "#050917",
+            color_inplay_light: "#FF0100",
+            color_primary_dark: "#FF0100",
+            color_bg_dark: "#050917",
+            color_text_dark: "#FFFFFF",
+            color_inplay_dark: "#FF0100",
+        },
+    }
+);
